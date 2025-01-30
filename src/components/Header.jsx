90ec15f0ad2d11363c478c80ec99e3bf3bcdf5a3
deepbm <deepbm27@gmail.com>
2025-01-30 21:27:59 +0900

    Header-style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,13 +14,14 @@ export default function Header() {
     setText(keyword || '');
   }, [keyword]);
   return (
-    <header>
-      <Link to='/'>
-        <BsYoutube />
-        <h1>Youtube</h1>
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to='/' className='flex items-center'>
+        <BsYoutube className='text-4xl text-brand' />
+        <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
         <input
+          className='w-7/12 p-2 outline-none bg-black text-gray-50'
           type='text'
           placeholder='search...'
           value={text}
@@ -28,7 +29,7 @@ export default function Header() {
             setText(e.target.value);
           }}
         />
-        <button>
+        <button className='bg-zinc-600 px-4'>
           <BsSearch />
         </button>
       </form>
